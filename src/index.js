678d37e96c0083b750fa241649155400aad7b321
Rom0105 <savosko.photo@gmail.com>
2021-08-20 19:16:44 +0300

    правки

--- src/index.js
+++ src/index.js
@@ -1,2 +1,57 @@
 import './sass/main.scss';
-import handlebars from 'handlebars';
+import cocktailslList from './templates/search-cocktails.hbs';
+import '@pnotify/core/dist/BrightTheme.css';
+import { error, alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+
+const refs = {
+  form: document.querySelector('#form'),
+  input: document.querySelector('.input-form'),
+  btn: document.querySelector('.btn'),
+  container: document.querySelector('.js-container'),
+};
+
+refs.form.addEventListener('submit', handlerSubmit);
+refs.btn.addEventListener('click', clearBtn);
+
+function handlerSubmit(e) {
+  e.preventDefault();
+  const value = refs.input.value;
+
+  clearMarkup();
+  clear();
+  clearBtn();
+  errorCocktail();
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`)
+    .then(response => response.json())
+    .then(cocktails => markup(cocktails.drinks))
+    .catch(err => console.log(err));
+}
+
+function clear() {
+  refs.input.value = '';
+}
+
+function markup(cocktails) {
+  refs.container.insertAdjacentHTML('beforeend', cocktailslList(cocktails));
+}
+
+function clearMarkup() {
+  refs.container.innerHTML = '';
+}
+
+function clearBtn() {
+  if (refs.input.value === '') {
+    refs.btn.setAttribute('disabled', 'disabled');
+  }
+  return;
+}
+
+function errorCocktail(cocktails) {
+  if ({ drinks: null }) {
+    error({
+      text: 'Сocktail not found!',
+      delay: 2000,
+    });
+    return;
+  }
+}
